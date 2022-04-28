8f7f0a9fe229b6852362a6485fde5038817e98f9
ioanat94 <ioanatiplea94@gmail.com>
2022-04-28 09:36:15 +0300

    Created custom component

--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const page = (
-    <div>
-        <img src='./react-logo.png' width='40px' alt='React Logo'></img>
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on Github</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </div>
-)
+function TemporaryName() {
+    return (
+        <div>
+            <img src='./react-logo.png' width='40px' alt='React Logo'></img>
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
+    )
+}
 
-ReactDOM.render(page, document.getElementById('root'));
+ReactDOM.render(<TemporaryName />, document.getElementById('root'));

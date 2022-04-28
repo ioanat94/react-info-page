ccc55730c189b27733a81ab7bf11bae6d50f39a8
ioanat94 <ioanatiplea94@gmail.com>
2022-04-28 09:18:26 +0300

    Created markup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 
+const page = (
+    <div>
+        <img src='./react-logo.png' width='40px' alt='React Logo'></img>
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
+)
+
+ReactDOM.render(page, document.getElementById('root'));

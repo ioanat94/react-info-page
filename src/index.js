87cf6872d22a351b8a9b88a9655928bcab39a511
ioanat94 <ioanatiplea94@gmail.com>
2022-04-28 10:28:49 +0300

    Added styling

--- src/index.js
+++ src/index.js
@@ -5,8 +5,13 @@ import './index.css';
 function Header() {
     return (
         <header>
-            <nav>
-                <img src='./react-logo.png' width='40px' alt='React Logo' />
+            <nav className='nav'>
+                <img src='./react-logo.png' alt='React Logo' className='nav-img' />
+                <ul className='nav-items'>
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
             </nav>
         </header>
     )
